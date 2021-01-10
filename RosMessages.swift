b04a42cdd26413ea92338e357eb915bc0dfc6564
Christophe Bedard <bedard.christophe@gmail.com>
2021-01-10 14:13:49 -0500

    Add definitions for sensor_msgs/{PointCloud2,PointField}
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- RosMessages.swift
+++ RosMessages.swift
@@ -31,6 +31,36 @@ struct sensor_msgs__Image : Encodable {
     var data: [UInt8]
 }
 
+/// sensor_msgs/PointField
+struct sensor_msgs__PointField : Encodable {
+    var name: String
+    var offset: UInt32
+    var datatype: UInt8
+    var count: UInt32
+    
+    static let DATATYPE_INT8: UInt8 = 1
+    static let DATATYPE_UINT8: UInt8 = 2
+    static let DATATYPE_INT16: UInt8 = 3
+    static let DATATYPE_UINT16: UInt8 = 4
+    static let DATATYPE_INT32: UInt8 = 5
+    static let DATATYPE_UINT32: UInt8 = 6
+    static let DATATYPE_FLOAT32: UInt8 = 7
+    static let DATATYPE_FLOAT64: UInt8 = 8
+}
+
+/// sensor_msgs/PointCloud2
+struct sensor_msgs__PointCloud2 : Encodable {
+    var header: std_msgs__Header
+    var height: UInt32
+    var width: UInt32
+    var fields: [sensor_msgs__PointField]
+    var is_bigendian: Bool
+    var point_step: UInt32
+    var row_step: UInt32
+    var data: [UInt8]
+    var is_dense: Bool
+}
+
 /// std_msgs/String
 struct std_msgs__String : Encodable {
     var data: String
